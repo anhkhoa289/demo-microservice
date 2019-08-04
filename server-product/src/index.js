1b12ea89110e853d5a766bbb99977917242dc489
khoa <khoa@khoas-MacBook-Pro.local>
2019-08-04 20:39:12 +0700

    demo for products

--- server-product/src/index.js
+++ server-product/src/index.js
@@ -1,8 +1,19 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
+app.use(cors())
 const port = 80
 
 app.get('/', (req, res) => res.send('Hello World! I\'m product'))
+app.get('/test/api', (req, res) => res.send({ success: true }))
+app.get('/test/products', (req, res) => res.send({
+  success: true,
+  products: [
+    { id: 'para', name: 'laptop' },
+    { id: 'tani', name: 'desktop' },
+    { id: 'liti', name: 'smartphone' }
+  ]
+}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
